5fd8d7d1520a1288f3e897c24ae8202df2010ac7
Alexvrlk <alex.varlakov@gmail.com>
2016-07-12 14:35:26 +0300

    array1 5lesson

--- jslesson5/array1.js
+++ jslesson5/array1.js
@@ -1,9 +1,22 @@
-function arrLast (AnyArray) {
-    return AnyArray[AnyArray.length-1];
+function arrLast(AnyArray) {
+    'use strict';
+    return AnyArray[AnyArray.length - 1];
 }
-var ar = ['sdf',1,2,4,5,'56','sasa'];
-console.log(arrLast(ar))
+var ar = ['sdf', 1, 2, 4, 5, '56', 'sasa'];
+console.log(arrLast(ar));
 
-function arrAdd (arr, any) {
-    
+function arrAdd(arr, any) {
+    'use strict';
+    arr.push(any);
+    return arr;
 }
+console.log(arrAdd([1, 2, 3, 4, 5], 'hihi'));
+
+var fruits = ['apple', 'orange'];
+fruits.push('kiwi');
+console.log(fruits);
+fruits[fruits.length - 2] = 'pear';
+console.log(fruits);
+console.log(fruits.shift());
+fruits.unshift('apricot', 'peach');
+console.log(fruits);
